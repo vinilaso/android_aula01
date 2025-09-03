1d54dc6357e2b31b66945f203761b40e11928006
vinilaso <vinicius.lanser@gmail.com>
2025-09-03 19:05:04 -0300

    Sorteador

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,25 +1,38 @@
 package com.example.myapplication;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textViewResultado;
+    EditText editTextMin,editTextMax;
+    Button button;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.hello);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        button = findViewById(R.id.button);
+        editTextMin = findViewById(R.id.edMin);
+        editTextMax = findViewById(R.id.edMax);
+        textViewResultado = findViewById(R.id.tvResultado);
+        button.setOnClickListener(v -> {
+            int min = Integer.parseInt(editTextMin.getText().toString());
+            int max = Integer.parseInt(editTextMax.getText().toString());
+            int sorteado = 0;
+            sorteado = (int) (Math.random() * (max-min)+min);
+            textViewResultado.setText(Integer.toString(sorteado));
         });
+
     }
 }
