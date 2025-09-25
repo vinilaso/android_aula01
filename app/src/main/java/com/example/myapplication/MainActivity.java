fd87fd8d340a09055163e2fd9ab9b9088b302d17
vinilaso <vinicius.lanser@gmail.com>
2025-09-24 21:22:01 -0300

    Projeto IMC

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,16 +13,45 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText _inputAltura;
+    private EditText _inputPeso;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        getString(R.string.hello);
+
+        _inputAltura = findViewById(R.id.editTextAltura);
+        _inputPeso = findViewById(R.id.editTextPeso);
+
+        setButtonListener();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void setButtonListener() {
+        Button submit = findViewById(R.id.buttonCalcular);
+        submit.setOnClickListener(c -> {
+
+            Bundle bundle = new Bundle();
+            bundle.putDouble("imc", calcularIMC());
+
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+            intent.putExtras(bundle);
+
+            startActivity(intent);
+        });
+    }
+
+    private Double calcularIMC() {
+        Double altura = Double.parseDouble(_inputAltura.getText().toString());
+        Double peso = Double.parseDouble(_inputPeso.getText().toString());
+
+        return peso / (altura * altura);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/ResultActivity.java
@@ -0,0 +1,32 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private TextView _result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_result);
+
+        _result = findViewById(R.id.textView);
+        _result.setText(String.valueOf(getIntent().getExtras().getDouble("imc")));
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.result), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
